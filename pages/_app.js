241d45d9ead24c0df20b48c8f83ba1b923d7239c
leandervdiepen <vandiepen.webdev@gmail.com>
2022-03-25 18:26:28 +0100

    add navigation

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
-import "../styles/components.css"
+import "../styles/components.css";
+import Navbar from "../components/Navigation";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
